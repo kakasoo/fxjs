5893990c9e8ccbac80464d9f5680c7b3e2332c95
kakasoo <kscodebase@gmail.com>
2021-06-18 14:59:30 +0900

    take 함수의 조합성을 위해 curry 적용

--- src/take.js
+++ src/take.js
@@ -1,12 +1,14 @@
 // suspended 상태인 iterable을 배열 형태로 변환해주는 함수
 
-const take = (l, iter) => {
+const curry = require("./curry");
+
+const take = curry((l, iter) => {
     const res = [];
     for (const item of iter) {
         res.push(item);
         if (res.length === l) return res;
     }
     return res;
-};
+});
 
 module.export = take;
