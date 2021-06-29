b09d5410f4156f00a80b312da63b16d80f56d4a4
kakasoo <kscodebase@gmail.com>
2021-06-29 17:43:44 +0900

    curry를 사용하여 go, pipe 내 활용도 증대

--- src/find.js
+++ src/find.js
@@ -1,7 +1,8 @@
+const curry = require("./curry");
 const go = require("./go");
 const filterL = require("./Lazy/filterL");
 const take = require("./take");
 
-const find = (f, iter) => go(iter, filterL(f), take(1), ([a]) => a);
+const find = curry((f, iter) => go(iter, filterL(f), take(1), ([a]) => a));
 
 module.exports = find;
