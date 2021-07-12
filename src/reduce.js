d6fb2293f46b2c0e9175278f05a9a057a5f7a3c4
kakasoo <kscodebase@gmail.com>
2021-07-12 15:05:50 +0900

    reduce가 promise를 처리할 수 있도록 변경

--- src/reduce.js
+++ src/reduce.js
@@ -1,15 +1,27 @@
 const curry = require("./curry");
 
+const go1 = (a, f) => (a instanceof Promise ? a.then(f) : f(a));
+
 const reduce = curry((f, acc, iter) => {
     if (!iter) {
         iter = acc[Symbol.iterator]();
         acc = iter.next().value;
+    } else {
+        iter = iter[Symbol.iterator]();
     }
 
-    for (const a of iter) {
-        acc = f(acc, a);
-    }
-    return acc;
+    // for (const a of iter) {
+    //     acc = acc instanceof Promise ? acc.then((acc) => f(acc, a)) : f(acc, a);
+    // }
+    // return acc;
+    return go1(acc, function recur() {
+        while (!(cur = iter.next()).done) {
+            const a = cur.value;
+            acc = f(acc, a);
+            if (acc instanceof Promise) return acc.then(recur);
+        }
+        return acc;
+    });
 });
 
 module.exports = reduce;
